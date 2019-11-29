35bc789a6a73699ca3d262fdd50a8779e4cac070
Sergey <sergey.bushnyak@sigrlami.eu>
2019-11-29 20:03:15 +0200

    add sync status method

--- src/PegNet/RPC/Api.hs
+++ src/PegNet/RPC/Api.hs
@@ -1,6 +1,53 @@
-module PegNet.RPC.Api
-    ( someFunc
-    ) where
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module PegNet.RPC.Api where
+
+import           Control.Concurrent
+import           Control.Exception                (bracket)
+import           Control.Monad.IO.Class
+import           Control.Remote.Monad.JSON
+import           Control.Remote.Monad.JSON.Client
+import           Control.Remote.Monad.JSON.Router
+import           Control.Remote.Monad.JSON.Trace
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Text
+import           Network.Socket                   (HostName, ServiceName,
+                                                   SocketType (Stream),
+                                                   addrAddress, addrFamily,
+                                                   addrProtocol, addrSocketType,
+                                                   close, connect, defaultHints,
+                                                   getAddrInfo, socket)
+
+import           PegNet.RPC.Types.SyncStatus
+
+--------------------------------------------------------------------------------
+
+endpoint  = "http://localhost:8070/v1"
+endpointRemote = "http://dev.pegnet.org/v1"
+
+-- | "get-sync-status"
+--   Return the current heights synced by pegnetd and the factomd it is communicating with
+reqGetSyncStatus :: RPC SyncStatus
+reqGetSyncStatus =
+  method "get-sync-status" None -- $ List [toJSON height]
+
+
+
+--------------------------------------------------------------------------------
+
+main = do
+  let s = weakSession (traceSendAPI "" $ clientSendAPI endpoint)
+  h <- send s $ do
+         h <- reqGetSyncStatus
+         return h
+  print h
